d501f58e92a6590ef50cf50cbde644749eae4646
Bernie Reiter <ockham@raz.or.at>
2019-04-23 18:57:41 +0600

    Simplify mapDispatchToProps

--- _inc/client/main.jsx
+++ _inc/client/main.jsx
@@ -326,14 +326,7 @@ export default connect(
 			rewindStatus: getRewindStatus( state ),
 		};
 	},
-	dispatch => ( {
-		setInitialState: () => {
-			return dispatch( setInitialState() );
-		},
-		clearUnsavedSettingsFlag: () => {
-			return dispatch( clearUnsavedSettingsFlag() );
-		},
-	} )
+	{ clearUnsavedSettingsFlag, setInitialState }
 )( withRouter( Main ) );
 
 /**
